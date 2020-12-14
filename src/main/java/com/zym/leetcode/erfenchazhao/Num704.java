2448ebffcd81d616a21ef7ed43ec3c1c339f3c53
zhucucao <709020520@qq.com>
2020-12-14 22:43:38 +0800

    Num704

--- src/main/java/com/zym/leetcode/erfenchazhao/Num704.java
+++ src/main/java/com/zym/leetcode/erfenchazhao/Num704.java
@@ -27,14 +27,25 @@ package com.zym.leetcode.erfenchazhao;
  * 链接：https://leetcode-cn.com/problems/binary-search
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
+/**
+ * https://labuladong.gitbook.io/algo/di-ling-zhang-bi-du-xi-lie/er-fen-cha-zhao-xiang-jie
+ */
 public class Num704 {
 
-    public int search(int[] nums, int target) {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 6;
+        search(nums,target);
+    }
+
+    public static int search(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            System.out.println(mid+":"+left);
           if(nums[mid] == target) {
               return mid;
           }else if(nums[mid] < target) {
