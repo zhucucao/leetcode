58f9fd2e6927d11ddc1615db7c5b86fd9ba381fe
zhucucao <709020520@qq.com>
2022-02-21 07:58:33 +0800

    83 删除排序链表中的重复元素

---
+++ src/main/java/com/zym/newleetcode/数组/leetcode83.java
@@ -0,0 +1,53 @@
+package com.zym.newleetcode.数组;
+
+import java.util.List;
+
+/*
+链表
+ */
+public class leetcode83 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null) {
+            int currentVal = cur.val;
+            int nextVal = cur.next.val;
+            if(currentVal == nextVal) {
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
+
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+}
