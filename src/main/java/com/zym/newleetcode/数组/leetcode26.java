abf8112a8ee2d791acbaba2710fd36bca4c4cd5c
zym <yaming.zhu@clarivate.com>
2022-02-21 17:44:03 +0800

    数组->快慢指针

--- src/main/java/com/zym/newleetcode/数组/leetcode26.java
+++ src/main/java/com/zym/newleetcode/数组/leetcode26.java
@@ -9,7 +9,7 @@ public class leetcode26 {
 
     public int removeDuplicates(int[] nums) {
         int slow = 0;
-        for(int fast = 0 ; fast < nums.length ; fast++) {
+        for(int fast = 1 ; fast < nums.length ; fast++) {
             if(nums[fast] != nums[slow]) {
                 slow++;
                 nums[slow] = nums[fast];
