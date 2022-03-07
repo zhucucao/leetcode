a7c8abe65ed09f00dd46bcf607f5227bac6cf969
zhucucao <709020520@qq.com>
2022-03-08 07:47:25 +0800

    leetcode48 -》数组 模拟

---
+++ src/main/java/com/zym/newleetcode/数组/模拟/leetcode48.java
@@ -0,0 +1,18 @@
+package com.zym.newleetcode.数组.模拟;
+
+import java.util.Arrays;
+
+public class leetcode48 {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        arr[0] = new int[]{1,2,3};
+        arr[1] = new int[]{4,5,6};
+        arr[2] = new int[]{7,8,9};
+
+        new leetcode48().rotate(arr);
+    }
+
+    public void rotate(int[][] matrix) {
+
+    }
+}
